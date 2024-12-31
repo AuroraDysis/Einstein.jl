6d88b96638d625e1661f7816888583fdb067b7fc
Documenter.jl <documenter@juliadocs.github.io>
2024-12-31 17:16:53 +0000

    build based on 10aeb19

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GRSuite","category":"page"},{"location":"#GRSuite","page":"Home","title":"GRSuite","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GRSuite.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GRSuite]","category":"page"}]
+}
