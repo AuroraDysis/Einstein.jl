c72b658721a66b686e86d14b3ee96f611cf3e9bb
Zhen Zhong <auroradysis@gmail.com>
2025-01-02 18:07:03 +0000

    fix docs

--- src/cheb/utils/bary.jl
+++ src/cheb/utils/bary.jl
@@ -14,44 +14,8 @@ Evaluate a polynomial interpolant using the barycentric interpolation formula.
 - `f`: Vector of function values at interpolation points
 - `x0`: Point at which to evaluate the interpolant
 
-# Returns
-- Interpolated value at x0
-
-# Mathematical Details
-The barycentric interpolation formula is:
-```math
-p(x) = \\begin{cases}
-f_j & \\text{if } x = x_j \\text{ for some } j \\\\
-\\frac{\\sum_{j=0}^{n-1} \\frac{w_j}{x-x_j}f_j}{\\sum_{j=0}^{n-1} \\frac{w_j}{x-x_j}} & \\text{otherwise}
-\\end{cases}
-```
-
-This formula provides a numerically stable way to evaluate the Lagrange interpolation
-polynomial. When used with Chebyshev points and their corresponding barycentric weights,
-it gives optimal interpolation properties.
-
-# Examples
-```julia
-# Set up interpolation points and weights
-n = 10
-x = cheb2_pts(Float64, n)
-w = cheb2_barywts(Float64, n)
-
-# Function to interpolate
-f = sin.(π .* x)
-
-# Evaluate interpolant at a point
-x0 = 0.5
-y = bary(w, x, f, x0)
-```
-
 # Reference
-
-- [salzer1972lagrangian](@citet*)
-- [doi:10.1137/1.9781611975949](@citet*)
 - [chebfun/@chebtech2/bary.m at master · chebfun/chebfun](https://github.com/chebfun/chebfun/blob/master/%40chebtech2/bary.m)
-
-See also: [`cheb1_barywts`](@ref), [`cheb1_pts`](@ref), [`cheb2_barywts`](@ref), [`cheb2_pts`](@ref)
 """
 function bary(
     w::VT1, x::VT2, f::VT3, x0::TR
