13bc3e75f11c07a1457d5c29f82e0daefc89509d
Zhen Zhong <auroradysis@gmail.com>
2025-01-02 16:50:17 +0000

    improve docs

--- src/cheb/cheb1/asmat.jl
+++ src/cheb/cheb1/asmat.jl
@@ -1,3 +1,12 @@
+"""
+    cheb1_amat([TR=Float64], n::Integer) -> Matrix{TR}
+
+Construct the analysis matrix A that transforms function values at Chebyshev points of the first kind to Chebyshev coefficients.
+
+# Arguments
+- `TR`: Element type (defaults to Float64)
+- `n`: Number of points/coefficients
+"""
 function cheb1_amat(::Type{TR}, n::TI) where {TR<:AbstractFloat,TI<:Integer}
     A = Array{Float64,2}(undef, n, n)
     op = Cheb1Vals2CoeffsOp(TR, n)
@@ -7,10 +16,15 @@ function cheb1_amat(::Type{TR}, n::TI) where {TR<:AbstractFloat,TI<:Integer}
     return A
 end
 
-function cheb1_amat(n::TI) where {TI<:Integer}
-    return cheb1_amat(Float64, n)
-end
+"""
+    cheb1_smat([TR=Float64], n::Integer) -> Matrix{TR}
 
+Construct the synthesis matrix S that transforms Chebyshev coefficients to function values at Chebyshev points of the first kind.
+
+# Arguments
+- `TR`: Element type (defaults to Float64)
+- `n`: Number of points/coefficients
+"""
 function cheb1_smat(::Type{TR}, n::TI) where {TR<:AbstractFloat,TI<:Integer}
     S = Array{Float64,2}(undef, n, n)
     op = Cheb1Coeffs2ValsOp(TR, n)
@@ -18,6 +32,10 @@ function cheb1_smat(::Type{TR}, n::TI) where {TR<:AbstractFloat,TI<:Integer}
         S[:, i] = op(OneElement(one(TR), i, n))
     end
     return S
+end
+
+function cheb1_amat(n::TI) where {TI<:Integer}
+    return cheb1_amat(Float64, n)
 end
 
 function cheb1_smat(n::TI) where {TI<:Integer}
--- src/cheb/cheb2/asmat.jl
+++ src/cheb/cheb2/asmat.jl
@@ -1,3 +1,12 @@
+"""
+    cheb2_amat([TR=Float64], n::Integer) -> Matrix{TR}
+
+Construct the analysis matrix A that transforms function values at Chebyshev points of the second kind to Chebyshev coefficients.
+
+# Arguments
+- `TR`: Element type (defaults to Float64)
+- `n`: Number of points/coefficients
+"""
 function cheb2_amat(::Type{TR}, n::TI) where {TR<:AbstractFloat,TI<:Integer}
     A = Array{Float64,2}(undef, n, n)
     op = Cheb2Vals2CoeffsOp(TR, n)
@@ -11,6 +20,15 @@ function cheb2_amat(n::TI) where {TI<:Integer}
     return cheb2_amat(Float64, n)
 end
 
+"""
+    cheb1_smat([TR=Float64], n::Integer) -> Matrix{TR}
+
+Construct the synthesis matrix S that transforms Chebyshev coefficients to function values at Chebyshev points of the second kind.
+
+# Arguments
+- `TR`: Element type (defaults to Float64)
+- `n`: Number of points/coefficients
+"""
 function cheb2_smat(::Type{TR}, n::TI) where {TR<:AbstractFloat,TI<:Integer}
     S = Array{Float64,2}(undef, n, n)
     op = Cheb2Coeffs2ValsOp(TR, n)
