22263e93faf753772d9abb904a5f907629e5037a
Zhen Zhong <auroradysis@gmail.com>
2025-01-02 19:51:15 +0000

    Update pts.jl

--- src/cheb/cheb1/pts.jl
+++ src/cheb/cheb1/pts.jl
@@ -67,13 +67,17 @@ export cheb1_pts
 
 @testset "cheb1_pts" begin
     for TR in [Float64, BigFloat]
+        tol = 10 * eps(TR)
         for intervals in [[-1, 1], [0, 1]]
             x_min = convert(TR, intervals[1])
             x_max = convert(TR, intervals[2])
-            @testset "$type, $intervals" begin
+            @testset "$TR, $intervals" begin
                 for n in 0:10
-                    @test cheb1_pts(type, n, x_min, x_max) ≈
-                        points(Chebyshev(x_min .. x_max), n)
+                    @test isapprox(
+                        cheb1_pts(TR, n, x_min, x_max),
+                        reverse(points(Chebyshev(x_min .. x_max), n)),
+                        atol=tol,
+                    )
                 end
             end
         end
