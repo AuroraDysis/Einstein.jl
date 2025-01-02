5b866e1275742a360d8acebd277e7e2bd031897e
Zhen Zhong <auroradysis@gmail.com>
2025-01-02 22:22:05 +0000

    fix docs

--- src/cheb/ultra/sphankel.jl
+++ src/cheb/ultra/sphankel.jl
@@ -6,6 +6,9 @@ using ToeplitzMatrices
 this forms a sparse Hankel matrix by forming it as an upside-
 down Toeplitz matrix. This is required by the ultraspherical multiplication
 operator.
+
+# References
+- [chebfun/@ultraS/sphankel.m at master · chebfun/chebfun](https://github.com/chebfun/chebfun/blob/master/%40ultraS/sphankel.m)
 """
 function ultra_sphankel(r::VT) where {VT<:AbstractVector{TR},TR<:AbstractFloat}
     return Hankel(r, OneElement(r[end], 1, length(r)))
