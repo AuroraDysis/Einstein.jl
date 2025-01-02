673b0badf6fd115d353dce93d4e17ac2485f3251
Zhen Zhong <auroradysis@gmail.com>
2025-01-02 20:58:16 +0000

    Update convertmat.jl

--- src/cheb/ultra/convertmat.jl
+++ src/cheb/ultra/convertmat.jl
@@ -1,48 +1,69 @@
 """
-    spconvert([TR=Float64], n::TI, λ::TR) where {TR<:AbstractFloat,TI<:Integer,TR<:Real}
+    ultra_spconvert([TR=Float64], n::TI, λ::TR) where {TR<:AbstractFloat,TI<:Integer,TR<:Real}
 
 Compute sparse representation for conversion operators.
 Returns the truncation of the operator that transforms \$C^{\\lambda}\$
 (Ultraspherical polynomials) to \$C^{\\lambda + 1}\$. The truncation gives
 back a matrix of size n x n.
 """
-function spconvert(::Type{TR}, n::TI, λ::TI) where {TI<:Integer,TR<:AbstractFloat}
+function ultra_spconvert(::Type{TR}, n::TI, λ::TI) where {TI<:Integer,TR<:AbstractFloat}
     λf = convert(TR, λ)
     if λ == 0
         half = one(TR) / 2
         dg = fill(half, n - 2)
         # Construct sparse matrix with diagonals at 0 and 2
-        return spdiagm(0 => [one(TR); half; dg], 2 => [zero(TR); zero(TR); -dg])
+        return spdiagm(0 => [one(TR); half; dg], 2 => -dg)
     else
-        dg = λf ./ (λf .+ (2:(n - 1)))
-        return spdiagm(0 => [one(TR); λf / (λf + 1); dg], 2 => [zero(TR); zero(TR); -dg])
+        dg = [λf / (λf + i) for i in 2:(n - 1)]
+        return spdiagm(0 => [one(TR); λf / (λf + 1); dg], 2 => -dg)
     end
 end
 
-function spconvert(n::TI, λ::TR) where {TR<:Real,TI<:Integer}
-    return spconvert(Float64, n, λ)
+function ultra_spconvert(n::TI, λ::TR) where {TR<:Real,TI<:Integer}
+    return ultra_spconvert(Float64, n, λ)
 end
 
 """
-    convertmat([TR=Float64], n::TI, K1::TI, K2::TI) where {TR<:AbstractFloat,TI<:Integer}
+    ultra_convertmat([TR=Float64], n::TI, K1::TI, K2::TI) where {TR<:AbstractFloat,TI<:Integer}
 
 Conversion matrix used in the ultraspherical spectral method.
 Returns N-by-N matrix realization of conversion operator between ultraspherical polynomial bases.
-Maps N coefficients from C^{(K1)} basis to C^{(K2 + 1)} basis.
-Returns identity matrix if K2 < K1.
+Maps N coefficients from C^{(K1)} basis to C^{(K2)} basis.
 """
-function convertmat(::Type{TR}, n::TI, K1::TI, K2::TI) where {TR<:AbstractFloat,TI<:Integer}
+function ultra_convertmat(
+    ::Type{TR}, n::TI, K1::TI, K2::TI
+) where {TR<:AbstractFloat,TI<:Integer}
+    @argcheck n >= 2 "n must be positive"
+    @argcheck K2 >= K1 "K2 must be greater than or equal to K1"
     S = sparse(I, n, n)
-    if K2 >= K1
-        for s in K1:K2
-            S = spconvert(TR, n, s) * S
-        end
+    for s in K1:(K2 - 1)
+        S = ultra_spconvert(TR, n, s) * S
     end
     return S
 end
 
-function convertmat(n::TI, K1::TR, K2::TR) where {TI<:Integer,TR<:Real}
-    return convertmat(Float64, n, K1, K2)
+function ultra_convertmat(n::TI, K1::TR, K2::TR) where {TI<:Integer,TR<:Real}
+    return ultra_convertmat(Float64, n, K1, K2)
 end
 
-export convertmat, spconvert
+export ultra_convertmat, ultra_spconvert
+
+@testset "ultra_convertmat" begin
+    for type in [Float64, BigFloat]
+        tol = 10 * eps(type)
+        for n in 2:10
+            for K1 in 1:3
+                for K2 in K1:3
+                    dom = -one(type) .. one(type)
+                    @test isapprox(
+                        ultra_convertmat(type, n, K1, K2),
+                        Conversion(Ultraspherical(K1, dom), Ultraspherical(K2, dom))[
+                            1:n, 1:n
+                        ],
+                        atol=tol,
+                    )
+                end
+            end
+        end
+    end
+end
