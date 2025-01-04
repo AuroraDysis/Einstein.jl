ef8f46ae9c338ad8f89406646f9f44c2c460184a
Zhen Zhong <auroradysis@gmail.com>
2025-01-04 20:31:08 +0000

    add hermiteop

--- src/fdm/fdmop.jl
+++ src/fdm/fdmop.jl
@@ -1,4 +1,4 @@
-struct FDMOp{T<:Real}
+struct FDMCentralOp{T<:Real}
     der_order::Integer
     acc_order::Integer
     num_coeffs::Integer
@@ -8,7 +8,7 @@ struct FDMOp{T<:Real}
 end
 
 # TODO: benchmark this and implement a more efficient version
-@inline function (op::FDMOp{T})(v::StridedVector{T}) where {T<:AbstractFloat}
+@inline function (op::FDMCentralOp{T})(v::StridedVector{T}) where {T<:AbstractFloat}
     return dot(op.wts, v) * op.one_over_dxn
 end
 
@@ -29,7 +29,54 @@ function fdm_centralop(
     num_coeffs = fdm_centralnum(der_order, acc_order)
     num_side = div(num_coeffs - 1, 2)
     wts = fdm_central(T, der_order, acc_order)
-    return FDMOp{T}(der_order, acc_order, num_coeffs, num_side, wts, one(T) / dx^der_order)
+    return FDMCentralOp{T}(
+        der_order, acc_order, num_coeffs, num_side, wts, one(T) / dx^der_order
+    )
 end
 
-export FDMOp, fdm_centralop
+struct FDMHermiteOp{T<:Real}
+    der_order::Integer
+    acc_order::Integer
+    num_coeffs::Integer
+    num_side::Integer
+    Dwts::Vector{T}
+    Ewts::Vector{T}
+    one_over_dxn::T
+    one_over_dxnm1::T
+end
+
+# TODO: benchmark this and implement a more efficient version
+function (op::FDMHermiteOp{T})(v::StridedVector{T}) where {T<:AbstractFloat}
+    return dot(op.Dwts, v) * op.one_over_dxn + dot(op.Ewts, v) * op.one_over_dxnm1
+end
+
+"""
+    fdm_hermiteop(der_order::Integer, acc_order::Integer, dx::T) where {T<:AbstractFloat}
+
+Create a Hermite finite difference operator with specified derivative order and accuracy.
+
+# Arguments
+- `der_order::Integer`: The order of the derivative to approximate
+- `acc_order::Integer`: The desired order of accuracy (must be even)
+- `dx::T`: Grid spacing
+"""
+function fdm_hermiteop(
+    der_order::Integer, acc_order::Integer, dx::T
+) where {T<:AbstractFloat}
+    num_coeffs = fdm_hermitenum(der_order, acc_order)
+    num_side = div(num_coeffs - 1, 2)
+    Dwts, Ewts = fdm_hermite(T, der_order, acc_order)
+    return FDMHermiteOp{T}(
+        der_order,
+        acc_order,
+        num_coeffs,
+        num_side,
+        Dwts,
+        Ewts,
+        one(T) / dx^der_order,
+        one(T) / dx^(der_order - 1),
+    )
+end
+
+export FDMCentralOp, fdm_centralop
+export FDMHermiteOp, fdm_hermiteop
--- test/fdm/fdmop.jl
+++ test/fdm/fdmop.jl
@@ -36,3 +36,42 @@
         @test op.wts[1] == op.wts[end]  # symmetry
     end
 end
+
+@testitem "fdm_hermiteop" begin
+    using LinearAlgebra, GRSuite.FDMSuite, Test
+
+    @testset "Constructor Tests" begin
+        dx = 0.1
+        # Basic construction
+        op = fdm_hermiteop(2, 4, dx)  # 2nd derivative, 4th order accuracy
+        @test op.der_order == 2
+        @test op.acc_order == 4
+    end
+
+    @testset "Operator Application" begin
+        # Test on quadratic function f(x) = x²
+        dx = 0.001
+        x = -10*dx:dx:10*dx
+        f = x .^ 2
+        center_idx = length(x) ÷ 2
+
+        # First derivative should give 2x
+        op1 = fdm_hermiteop(1, 4, dx)
+        df = op1(f[center_idx-op1.num_side:center_idx+op1.num_side])
+        @test df ≈ 2 .* x[center_idx]
+
+        # Second derivative should give 2
+        op2 = fdm_hermiteop(2, 4, dx)
+        d2f = op2(f[center_idx-op2.num_side:center_idx+op2.num_side])
+        @test d2f ≈ 2.0
+    end
+
+    @testset "Operator Properties" begin
+        op = fdm_hermiteop(2, 4, 0.1)
+
+        # Test stencil properties
+        @test length(op.wts) == 5  # 5-point stencil for 2nd der, 4th order
+        @test sum(op.wts) < 10 * eps(Float64)  # sum of weights should be close to zero
+        @test op.wts[1] == op.wts[end]  # symmetry
+    end
+end
