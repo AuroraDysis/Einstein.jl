62a3c7822384d68ad8db2e9d1e9c4ad2652fedfd
Zhen Zhong <auroradysis@gmail.com>
2025-01-05 14:12:38 +0000

    add tests

--- test/utils/sws.jl
+++ test/utils/sws.jl
@@ -12,3 +12,21 @@
     eidx = sws_eigvalidx(s, l, m)
     @test evals[eidx] ≈ eres
 end
+
+@testitem "SWSFun" begin
+    using Integrals, GRSuite, Test
+
+    a = 0.7
+    s = 2
+    l = 2
+    m = 2
+    ω = 0.532600243551018 - 0.08079287315500905im
+
+    tol = 10 * eps(Float64)
+    f = SWSFun{Float64}(s, a * ω, m, l, 25)
+    fi(x, p) = f(x) * conj(f(x)) * sin(x)
+    domain = (0, π)
+    prob = IntegralProblem(fi, domain)
+    sol = solve(prob, QuadGKJL())
+    @test isapprox(sol.u, 1, atol=tol)
+end
