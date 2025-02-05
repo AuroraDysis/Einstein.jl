f942ed0084f1a587a8e2661ce1cf4f75057c85ff
Zhen Zhong <auroradysis@gmail.com>
2025-02-05 06:24:32 +0000

    clean code

--- src/cheb/cheb2/pts.jl
+++ src/cheb/cheb2/pts.jl
@@ -1,6 +1,6 @@
 @doc raw"""
-    cheb2_pts([T=Float64], n::Integer) where {T<:AbstractFloat}
-    cheb2_pts([T=Float64], n::Integer, x_min::T, x_max::T) where {T<:AbstractFloat}
+    cheb2_pts([TF=Float64], n::Integer) where {TF<:AbstractFloat}
+    cheb2_pts([TF=Float64], n::Integer, x_min::TF, x_max::TF) where {TF<:AbstractFloat}
 
 Generate Chebyshev points of the 1st kind.
 
@@ -15,7 +15,7 @@ x_{\mathrm{mapped}} = \frac{x_{\mathrm{max}} + x_{\mathrm{min}}}{2} + \frac{x_{\
 ```
 
 # Arguments
-- `T`: Type parameter for the grid points (e.g., Float64)
+- `TF`: Type parameter for the grid points (e.g., Float64)
 - `n`: Number of points
 - `x_min`: (Optional) Lower bound of the mapped interval
 - `x_max`: (Optional) Upper bound of the mapped interval
@@ -23,19 +23,19 @@ x_{\mathrm{mapped}} = \frac{x_{\mathrm{max}} + x_{\mathrm{min}}}{2} + \frac{x_{\
 # References
 - [chebfun/@chebtech2/chebpts.m at master · chebfun/chebfun](https://github.com/chebfun/chebfun/blob/master/%40chebtech2/chebpts.m)
 """
-function cheb2_pts(::Type{T}, n::Integer) where {T<:AbstractFloat}
+function cheb2_pts(::Type{TF}, n::Integer) where {TF<:AbstractFloat}
     @argcheck n >= 0 "n must be nonnegative"
 
     if n == 0
-        return T[]
+        return TF[]
     elseif n == 1
-        return [zero(T)]
+        return [zero(TF)]
     end
 
-    x_grid = Vector{T}(undef, n)
+    x_grid = Vector{TF}(undef, n)
 
     nm1 = n - 1
-    pi_over_2nm1 = convert(T, π) / (2 * nm1)
+    pi_over_2nm1 = convert(TF, π) / (2 * nm1)
 
     @inbounds for i in 0:nm1
         k = -nm1 + 2i  # Creates range -m:2:m
@@ -50,8 +50,8 @@ function cheb2_pts(n::Integer)
 end
 
 # Mapped version documentation is inherited from the main docstring
-function cheb2_pts(::Type{T}, n::Integer, x_min::T, x_max::T) where {T<:AbstractFloat}
-    x_grid = cheb2_pts(T, n)
+function cheb2_pts(::Type{TF}, n::Integer, x_min::TF, x_max::TF) where {TF<:AbstractFloat}
+    x_grid = cheb2_pts(TF, n)
 
     a = (x_max + x_min) / 2
     b = (x_max - x_min) / 2
