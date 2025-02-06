573d20ddd0fce0b7e5f58efd821b66a4ddf3df3a
Zhen Zhong <auroradysis@gmail.com>
2025-02-06 19:17:10 +0000

    clean code

--- src/cheb/cheb1/grid.jl
+++ src/cheb/cheb1/grid.jl
@@ -23,3 +23,72 @@ Base.size(grid::ChebyshevGaussGrid) = (grid.n,)
 Base.@propagate_inbounds Base.getindex(grid::ChebyshevGaussGrid, i::Integer) = grid.grid[i]
 
 export ChebyshevGaussGrid
+
+@doc raw"""
+    cheb1_pts([T=Float64], n::Integer) where {T<:AbstractFloat}
+    cheb1_pts([T=Float64], n::Integer, x_min::T, x_max::T) where {T<:AbstractFloat}
+
+Generate Chebyshev points of the 2nd kind.
+
+For the standard interval [-1,1]:
+```math
+x_k = -\cos\left(\frac{(2k + 1)\pi}{2n}\right), \quad k = 0,1,\ldots,n-1
+```
+
+For mapped interval [x_min,x_max]:
+```math
+x_{\mathrm{mapped}} = \frac{x_{\mathrm{max}} + x_{\mathrm{min}}}{2} + \frac{x_{\mathrm{min}} - x_{\mathrm{max}}}{2}x_k
+```
+
+# Arguments
+- `T`: Type parameter for the grid points (e.g., Float64)
+- `n`: Number of points
+- `x_min`: (Optional) Lower bound of the mapped interval
+- `x_max`: (Optional) Upper bound of the mapped interval
+
+# References
+- [chebfun/@chebtech1/chebpts.m at master · chebfun/chebfun](https://github.com/chebfun/chebfun/blob/master/%40chebtech1/chebpts.m)
+"""
+function cheb1_pts(::Type{T}, n::Integer) where {T<:AbstractFloat}
+    @argcheck n >= 0 "n must be nonnegative"
+
+    if n == 0
+        return T[]
+    elseif n == 1
+        return [zero(T)]
+    end
+
+    x_grid = Vector{T}(undef, n)
+
+    # Use symmetric indexing for better numerical properties
+    pi_over_2n = convert(T, π) / (2 * n)
+    @inbounds begin
+        for i in 1:n
+            k = -n + 2i - 1
+            x_grid[i] = sin(k * pi_over_2n)
+        end
+    end
+
+    return x_grid
+end
+
+function cheb1_pts(n::Integer)
+    return cheb1_pts(Float64, n)
+end
+
+# Mapped version
+function cheb1_pts(::Type{T}, n::Integer, x_min::T, x_max::T) where {T<:AbstractFloat}
+    x_grid = cheb1_pts(T, n)
+
+    a = (x_max + x_min) / 2
+    b = (x_max - x_min) / 2
+    @.. x_grid = a + b * x_grid
+
+    return x_grid
+end
+
+function cheb1_pts(n::Integer, x_min::Float64, x_max::Float64)
+    return cheb1_pts(Float64, n, x_min, x_max)
+end
+
+export cheb1_pts
--- src/cheb/cheb2/grid.jl
+++ src/cheb/cheb2/grid.jl
@@ -1,3 +1,71 @@
+@doc raw"""
+    cheb2_pts([TF=Float64], n::Integer) where {TF<:AbstractFloat}
+    cheb2_pts([TF=Float64], n::Integer, x_min::TF, x_max::TF) where {TF<:AbstractFloat}
+
+Generate Chebyshev points of the 1st kind.
+
+For the standard interval [-1,1]:
+```math
+x_k = -\cos\left(\frac{k\pi}{n-1}\right), \quad k = 0,1,\ldots,n-1
+```
+
+For mapped interval [x_min,x_max]:
+```math
+x_{\mathrm{mapped}} = \frac{x_{\mathrm{max}} + x_{\mathrm{min}}}{2} + \frac{x_{\mathrm{min}} - x_{\mathrm{max}}}{2}x_k
+```
+
+# Arguments
+- `TF`: Type parameter for the grid points (e.g., Float64)
+- `n`: Number of points
+- `x_min`: (Optional) Lower bound of the mapped interval
+- `x_max`: (Optional) Upper bound of the mapped interval
+
+# References
+- [chebfun/@chebtech2/chebpts.m at master · chebfun/chebfun](https://github.com/chebfun/chebfun/blob/master/%40chebtech2/chebpts.m)
+"""
+function cheb2_pts(::Type{TF}, n::Integer) where {TF<:AbstractFloat}
+    @argcheck n >= 0 "n must be nonnegative"
+
+    if n == 0
+        return TF[]
+    elseif n == 1
+        return [zero(TF)]
+    end
+
+    x_grid = Vector{TF}(undef, n)
+
+    nm1 = n - 1
+    pi_over_2nm1 = convert(TF, π) / (2 * nm1)
+
+    @inbounds for i in 0:nm1
+        k = -nm1 + 2i  # Creates range -m:2:m
+        x_grid[i + 1] = sin(k * pi_over_2nm1)
+    end
+
+    return x_grid
+end
+
+function cheb2_pts(n::Integer)
+    return cheb2_pts(Float64, n)
+end
+
+# Mapped version documentation is inherited from the main docstring
+function cheb2_pts(::Type{TF}, n::Integer, x_min::TF, x_max::TF) where {TF<:AbstractFloat}
+    x_grid = cheb2_pts(TF, n)
+
+    a = (x_max + x_min) / 2
+    b = (x_max - x_min) / 2
+    @.. x_grid = a + b * x_grid
+
+    return x_grid
+end
+
+function cheb2_pts(n::Integer, x_min::Float64, x_max::Float64)
+    return cheb2_pts(Float64, n, x_min, x_max)
+end
+
+export cheb2_pts
+
 """
     ChebyshevLobattoGrid{TF} <: AbstractGrid{TF}
 
@@ -22,6 +90,7 @@ end
 
 Base.length(grid::ChebyshevLobattoGrid) = grid.n
 Base.size(grid::ChebyshevLobattoGrid) = (grid.n,)
-Base.@propagate_inbounds Base.getindex(grid::ChebyshevLobattoGrid, i::Integer) = grid.grid[i]
+Base.@propagate_inbounds Base.getindex(grid::ChebyshevLobattoGrid, i::Integer) =
+    grid.grid[i]
 
 export ChebyshevLobattoGrid
