be69654fc525275e214fa6bbcbe66503198b8626
Zhen Zhong <auroradysis@gmail.com>
2025-02-07 02:35:47 +0000

    Update grid.jl

--- src/fdm/grid.jl
+++ src/fdm/grid.jl
@@ -55,7 +55,7 @@ function fdm_grid(x_min::TF, x_max::TF, dx::TF) where {TF<:AbstractFloat}
 
     n = round(Int, (x_max - x_min) / dx) + 1
     x_grid_end = x_min + (n - 1) * dx
-    @argcheck (x_max - x_grid_end) < 10 * eps(T) "Grid endpoint mismatch: |x_max - x_grid_end| = $(abs(x_max - x_grid_end)) exceeds tolerance ($(10 * eps(T))). Consider adjusting dx to ensure x_max is reached precisely."
+    @argcheck (x_max - x_grid_end) < 10 * eps(TF) "Grid endpoint mismatch: |x_max - x_grid_end| = $(abs(x_max - x_grid_end)) exceeds tolerance ($(10 * eps(TF))). Consider adjusting dx to ensure x_max is reached precisely."
 
     return UniformGrid{TF}(x_min, x_max, n)
 end
