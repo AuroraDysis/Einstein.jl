d49c11457b34c5d23c0afb087b0590f03ad1fbff
Zhen Zhong <auroradysis@gmail.com>
2025-02-11 02:13:34 +0000

    Create synthesis.jl

---
+++ src/cheb/synthesis.jl
@@ -0,0 +1,24 @@
+abstract type AbstractChebyshevSynthesisImplementationementation end
+
+struct ChebyshevSynthesis{
+    TF<:AbstractFloat,TSynthesisImpl<:AbstractChebyshevSynthesisImplementation
+}
+    grid::ChebyshevGrid{TF}
+    synthesis::TSynthesisImpl
+
+    function ChebyshevSynthesis(grid::ChebyshevGrid{TF}) where {TF<:AbstractFloat}
+        if grid.type == ChebyshevNode.FirstKind
+            synthesis = ChebyshevFirstKindSynthesis{TF}(length(grid))
+        elseif grid.type == ChebyshevNode.SecondKind
+            synthesis = ChebyshevSecondKindSynthesis{TF}(length(grid))
+        else
+            throw(
+                ArgumentError(
+                    "kind must be either ChebyshevNode.FirstKind or ChebyshevNode.SecondKind",
+                ),
+            )
+        end
+
+        return new{TF,typeof(synthesis)}(grid, synthesis)
+    end
+end
