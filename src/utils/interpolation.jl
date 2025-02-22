1af6c964522c8db1e0020f62fa9a58d940c1ff4e
Zhen Zhong <auroradysis@gmail.com>
2025-02-22 18:34:24 +0000

    Update interpolation.jl

--- src/utils/interpolation.jl
+++ src/utils/interpolation.jl
@@ -16,6 +16,16 @@ function barycentric_weights(x::AbstractRange{TF}) where {TF<:AbstractFloat}
     return TF[(-1)^j * binomial(n, j) for j in 0:n]
 end
 
+"""
+    barycentric_interpolate(x::TF, points::AbstractVector{TF}, values::AbstractVector{TFC}, weights::Vector{TF}) where {TF<:AbstractFloat,TFC<:Union{TF,Complex{TF}}
+
+Evaluate the value of the barycentric interpolation formula with nodes `points`, function values `values`
+and barycentric weights `weights` at point `x`.
+
+# References
+- [chebfun/@chebtech2/bary.m at master · chebfun/chebfun](https://github.com/chebfun/chebfun/blob/master/%40chebtech2/bary.m)
+- [Berrut2004](@citet*)
+"""
 function barycentric_interpolate(
     x::TF, points::AbstractVector{TF}, values::AbstractVector{TFC}, weights::Vector{TF}
 ) where {TF<:AbstractFloat,TFC<:Union{TF,Complex{TF}}}
@@ -57,7 +67,12 @@ Evaluate the interpolant at point `x` for function values.
 """
 struct BarycentricInterpolation{TF<:AbstractFloat}
     points::AbstractVector{TF}   # Grid points
-    weights::AbstractVector{TF}        # Barycentric weights
+    weights::AbstractVector{TF}  # Barycentric weights
+
+    function BarycentricInterpolation(points::AbstractVector{TF}) where {TF<:AbstractFloat}
+        weights = barycentric_weights(points)
+        return new{TF}(points, weights)
+    end
 end
 
 function (itp::BarycentricInterpolation{TF})(
