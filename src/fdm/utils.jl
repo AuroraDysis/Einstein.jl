48f420bac926dfa5df16c0ec6cf0e6e4fd48b241
Zhen Zhong <auroradysis@gmail.com>
2025-03-05 00:39:27 +0000

    Update utils.jl

--- src/fdm/utils.jl
+++ src/fdm/utils.jl
@@ -11,6 +11,8 @@ Calculate the number of coefficients needed for central FDM stencil.
 - [Finite difference coefficient - Wikipedia](https://en.wikipedia.org/wiki/Finite_difference_coefficient)
 """
 function fdm_central_width(derivative_order::Integer, accuracy_order::Integer)
+    @argcheck accuracy_order % 2 == 0 "Only even orders are supported for central FDM stencils."
+
     # https://github.com/maroba/findiff/blob/master/findiff/coefs.py
     # coefficients number for central difference
     return 2 * div(derivative_order + 1, 2) - 1 + accuracy_order
@@ -29,6 +31,16 @@ Calculate the number of coefficients needed for Hermite FDM stencil.
 - [fornberg2021algorithm](@citet*)
 """
 function fdm_hermite_width(derivative_order::Integer, accuracy_order::Integer)
+    @argcheck derivative_order >= 2 "Only derivative order greater than or equal to 2 are supported for Hermite-type finite difference."
+
+    if mod(div(derivative_order, 2), 2) == 1
+        # accuracy_order must be 4,8,12... for der order 2,3,6,7,10,11...
+        @argcheck accuracy_order % 4 == 0 "Only accuracy_order % 4 == 0 are supported for Hermite-type finite difference with der order 2,3,6,7,10,11..."
+    else
+        # accuracy_order must be 2,6,10... for der order 4,5,8,9,12...
+        @argcheck accuracy_order % 4 == 2 "Only accuracy_order % 4 == 2 are supported for Hermite-type finite difference with der order 4,5,8,9,12..."
+    end
+
     return div(derivative_order, 2) + div(accuracy_order, 2)
 end
 
