c95e896904f2782c07d681fd4b97627299662688
Zhen Zhong <auroradysis@gmail.com>
2025-04-01 08:05:05 +0100

    dev

--- src/fdm/grid.jl
+++ src/fdm/grid.jl
@@ -1,44 +1,15 @@
-"""
-    UniformGrid{TF} <: AbstractGrid{TF}
+function fdm_uniform_grid(lower_bound::TF, upper_bound::TF, dx::TF)
+    @argcheck upper_bound > lower_bound "Invalid interval"
+    @argcheck dx > 0 "Spacing must be positive"
 
-Uniform grid with constant spacing.
+    n = round(Int, (upper_bound - lower_bound) / dx) + 1
+    precise_upper_bound = lower_bound + (n - 1) * dx
 
-# Arguments
-- lower_bound::TF: Lower bound of the grid
-- upper_bound::TF: Upper bound of the grid
-- dx::TF: Grid spacing
-"""
-struct UniformGrid{TF<:AbstractFloat} <: AbstractGrid{TF}
-    lower_bound::TF # lower bound of the grid
-    upper_bound::TF # upper bound of the grid
-    data::StepRangeLen{TF}
+    # relative error check
+    rtol = abs(precise_upper_bound - upper_bound) / abs(upper_bound)
+    @argcheck rtol < typetol(TF) "Grid endpoint mismatch: |upper_bound - precise_upper_bound| = $(abs(upper_bound - precise_upper_bound)) exceeds tolerance ($(10 * eps(TF))). Consider adjusting dx to ensure upper_bound is reached precisely."
 
-    function UniformGrid(lower_bound::TF, upper_bound::TF, dx::TF) where {TF<:AbstractFloat}
-        @argcheck upper_bound > lower_bound "Invalid interval"
-        @argcheck dx > 0 "Spacing must be positive"
-
-        n = round(Int, (upper_bound - lower_bound) / dx) + 1
-        precise_upper_bound = lower_bound + (n - 1) * dx
-        @argcheck (upper_bound - precise_upper_bound) < 10 * eps(TF) "Grid endpoint mismatch: |upper_bound - precise_upper_bound| = $(abs(upper_bound - precise_upper_bound)) exceeds tolerance ($(10 * eps(TF))). Consider adjusting dx to ensure upper_bound is reached precisely."
-
-        data = range(lower_bound, upper_bound, n)
-
-        return new{TF}(lower_bound, upper_bound, data)
-    end
+    return range(lower_bound, upper_bound, n)
 end
 
-Base.length(grid::UniformGrid) = length(grid.data)
-Base.step(grid::UniformGrid) = step(grid.data)
-Base.size(grid::UniformGrid) = size(grid.data)
-Base.@propagate_inbounds Base.getindex(grid::UniformGrid, i) = grid.data[i]
-Base.keys(grid::UniformGrid) = keys(grid.data)
-Base.@propagate_inbounds function Base.iterate(grid::UniformGrid, state...)
-    return iterate(grid.data, state...)
-end
-Base.firstindex(grid::UniformGrid) = firstindex(grid.data)
-Base.lastindex(grid::UniformGrid) = lastindex(grid.data)
-Base.eltype(::Type{UniformGrid{TF}}) where {TF<:AbstractFloat} = TF
-Base.IndexStyle(::Type{UniformGrid}) = IndexLinear()
-Base.diff(grid::UniformGrid) = Fill(step(grid.data), length(grid.data) - 1)
-
-export UniformGrid
+export fdm_uniform_grid
--- test/fdm/integrate.jl
+++ test/fdm/integrate.jl
@@ -2,7 +2,7 @@
     using Einstein.FiniteDifferenceSuite, Test
 
     dx = 0.01
-    x = UniformGrid(0.0, 1.0, dx)
+    x = fdm_uniform_grid(0.0, 1.0, dx)
     fx = @. sin(2π * x)
     integral = fdm_integrate_simpson(fx, dx)
     @test integral ≈ 0.0 atol = 1e-14
@@ -12,7 +12,7 @@ end
     using Einstein.FiniteDifferenceSuite, Test
 
     dx = 0.01
-    x = UniformGrid(0.0, 1.0, dx)
+    x = fdm_uniform_grid(0.0, 1.0, dx)
     fx = @. sin(2π * x)
     integral = fdm_integrate_trapezoidal(fx, dx)
     @test integral ≈ 0.0 atol = 1e-14
