505f0c2c174bcfc44edb41cbcd3ccc2e1b0f2fc7
Zhen Zhong <auroradysis@gmail.com>
2025-04-01 15:26:31 +0100

    Update cheb.jl

--- src/cheb/cheb.jl
+++ src/cheb/cheb.jl
@@ -13,17 +13,8 @@ using SparseArrays
 using BandedMatrices
 using ToeplitzMatrices: Toeplitz, Hankel
 
-include("grid.jl")
-include("angles.jl")
 include("interpolation.jl")
-include("analysis.jl")
-include("synthesis.jl")
-include("analysis_matrix.jl")
-include("synthesis_matrix.jl")
-include("differentiation_matrix.jl")
-include("integration_matrix.jl")
 include("dissipation.jl")
-include("quadrature_weights.jl")
 
 include("utils/fft.jl")
 include("utils/barycentric_differentiation_matrix.jl")
@@ -32,16 +23,8 @@ include("utils/coeffs_eval.jl")
 include("utils/coeffs_integrate.jl")
 include("utils/coeffs_integration_matrix.jl")
 
-include("chebtech2/points.jl")
-include("chebtech2/angles.jl")
-include("chebtech2/synthesis.jl")
-include("chebtech2/analysis.jl")
-include("chebtech2/barycentric_weights.jl")
-include("chebtech2/quadrature_weights.jl")
-include("chebtech2/differentiation_matrix.jl")
-include("chebtech2/analysis_matrix.jl")
-include("chebtech2/synthesis_matrix.jl")
-include("chebtech2/integration_matrix.jl")
+include("grid/gauss_chebyshev/grid.jl")
+include("grid/gauss_chebyshev_lobatto/grid.jl")
 
 # Rectangular spectral collocation
 include("rect/differentiation_matrix.jl")
