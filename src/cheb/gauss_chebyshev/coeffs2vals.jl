75102fd199452ba369dd73815772d4bff4aa2e26
Zhen Zhong <auroradysis@gmail.com>
2025-04-02 00:33:03 +0100

    Update coeffs2vals.jl

--- src/cheb/gauss_chebyshev/coeffs2vals.jl
+++ src/cheb/gauss_chebyshev/coeffs2vals.jl
@@ -15,32 +15,32 @@ values = op(coeffs)
 - [chebfun/@chebtech1/coeffs2vals.m at master · chebfun/chebfun](https://github.com/chebfun/chebfun/blob/master/%40chebtech1/coeffs2vals.m)
 """
 struct GaussChebyshevCoeffs2ValsCache{TF<:AbstractFloat,TPlan<:Plan{Complex{TF}}}
-    w::Vector{Complex{TF}}
+    weights::Vector{Complex{TF}}
     tmp::Vector{Complex{TF}}
-    vals::Vector{Complex{TF}}
-    real_vals::Vector{TF}
+    complex_values::Vector{Complex{TF}}
+    real_values::Vector{TF}
     fft_plan::TPlan
 
     function GaussChebyshevCoeffs2ValsCache{TF}(n::Integer) where {TF<:AbstractFloat}
         # Precompute weights
-        w = Vector{Complex{TF}}(undef, 2n)
+        weights = Vector{Complex{TF}}(undef, 2n)
         @inbounds begin
             half = one(TF) / 2
             m_im_pi_over_2n = -im * convert(TF, π) / (2n)
             for k in 0:(n - 1)
-                w[k + 1] = exp(k * m_im_pi_over_2n) * half
+                weights[k + 1] = exp(k * m_im_pi_over_2n) * half
             end
-            w[1] *= 2
-            w[n + 1] = 0
+            weights[1] *= 2
+            weights[n + 1] = 0
             for k in (n + 1):(2n - 1)
-                w[k + 1] = -exp(k * m_im_pi_over_2n) * half
+                weights[k + 1] = -exp(k * m_im_pi_over_2n) * half
             end
         end
         tmp = Vector{Complex{TF}}(undef, 2n)
-        vals = Vector{Complex{TF}}(undef, n)
-        real_vals = Vector{TF}(undef, n)
+        complex_values = Vector{Complex{TF}}(undef, n)
+        real_values = Vector{TF}(undef, n)
         fft_plan = plan_fft_measure!(tmp)
-        return new{TF,typeof(fft_plan)}(w, tmp, vals, real_vals, fft_plan)
+        return new{TF,typeof(fft_plan)}(weights, tmp, complex_values, real_values, fft_plan)
     end
 end
 
@@ -52,17 +52,17 @@ function (op::GaussChebyshevCoeffs2ValsCache{TF})(
     n = length(coeffs)
     if n <= 1
         if type_is_float
-            op.real_vals .= coeffs
-            return op.real_vals
+            op.real_values .= coeffs
+            return op.real_values
         else
-            op.vals .= coeffs
-            return op.vals
+            op.complex_values .= coeffs
+            return op.complex_values
         end
     end
 
-    w = op.w
+    weights = op.weights
     tmp = op.tmp
-    vals = op.vals
+    complex_values = op.complex_values
     fft_plan = op.fft_plan
 
     # Check for symmetry
@@ -83,13 +83,13 @@ function (op::GaussChebyshevCoeffs2ValsCache{TF})(
     end
 
     @inbounds begin
-        tmp .*= w
+        tmp .*= weights
         fft_plan * tmp
     end
 
     # Extract values
     @inbounds for i in 1:n
-        vals[i] = tmp[n - i + 1]
+        complex_values[i] = tmp[n - i + 1]
     end
 
     # Enforce symmetry if needed
@@ -98,24 +98,24 @@ function (op::GaussChebyshevCoeffs2ValsCache{TF})(
         @inbounds for i in 1:div(n, 2)
             j = n - i + 1
             if isEven
-                s = vals[i] + vals[j]
-                vals[i] = half * s
-                vals[j] = half * s
+                s = complex_values[i] + complex_values[j]
+                complex_values[i] = half * s
+                complex_values[j] = half * s
             else
-                d = vals[i] - vals[j]
-                vals[i] = half * d
-                vals[j] = -half * d
+                d = complex_values[i] - complex_values[j]
+                complex_values[i] = half * d
+                complex_values[j] = -half * d
             end
         end
     end
 
     if type_is_float
         @inbounds for k in 1:n
-            op.real_vals[k] = real(op.vals[k])
+            op.real_values[k] = real(op.complex_values[k])
         end
-        return op.real_vals
+        return op.real_values
     else
-        return op.vals
+        return op.complex_values
     end
 end
 
