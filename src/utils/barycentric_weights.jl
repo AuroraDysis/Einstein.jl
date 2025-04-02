e6f7e7340c54de325e3e4dfff0233b83b5294088
Zhen Zhong <auroradysis@gmail.com>
2025-04-02 17:39:27 +0100

    improve barycentric_weights

--- src/utils/barycentric_weights.jl
+++ src/utils/barycentric_weights.jl
@@ -6,6 +6,8 @@
 Computes barycentric weights for the nodes `x`.
 Alternatively, for $n + 1$ equidistant nodes, the weights can be computed by passing `n`.
 
+The weights are scaled such that their infinity norm equals 1.
+
 # References
 - [Berrut2004](@citet*)
 """
@@ -14,7 +16,32 @@ function barycentric_weights(x::AbstractVector{TF}) where {TF<:AbstractFloat}
         @argcheck length(x) > 1 "x must have at least two elements"
     end
 
-    return TF[inv(prod(x[i] - x[j] for j in eachindex(x) if j != i)) for i in eachindex(x)]
+    weights = similar(x)
+
+    x_min, x_max = extrema(x)
+    if x_min ≈ x_max
+        @warn "Input points are not distinct. Returning NaN weights."
+        fill!(weights, NaN)
+        return weights
+    end
+
+    # Capacity of the interval - helps prevent underflow/overflow
+    c = 4 / (x_max - x_min)
+
+    diff = similar(x)
+    for i in eachindex(x)
+        @.. diff = c * (x[i] - x)
+        diff[i] = 1
+
+        # Log-Sum-Exp trick to avoid underflow/overflow
+        prod_sign = prod(sign.(diff))
+        @.. diff = log(abs(diff))
+        weights[i] = 1 / (prod_sign * exp(sum(diff)))
+    end
+
+    weights ./= norm(weights, Inf)
+
+    return weights
 end
 
 function barycentric_weights(x::AbstractRange{TF}) where {TF<:AbstractFloat}
@@ -27,7 +54,10 @@ function barycentric_weights(::Type{TF}, n::Integer) where {TF<:AbstractFloat}
         @argcheck n > 0 "n must be a positive integer"
     end
 
-    return TF[(-1)^j * binomial(n, j) for j in 0:n]
+    norm_factor = binomial(n, n ÷ 2)
+    weights = TF[(-1)^j * binomial(n, j)//norm_factor for j in 0:n]
+
+    return weights
 end
 
 export barycentric_weights
