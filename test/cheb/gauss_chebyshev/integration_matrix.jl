33139c2db8eb2c51228195b67d9c70744b3a884b
Zhen Zhong <auroradysis@gmail.com>
2025-04-02 03:19:49 +0100

    Update integration_matrix.jl

--- test/cheb/gauss_chebyshev/integration_matrix.jl
+++ test/cheb/gauss_chebyshev/integration_matrix.jl
@@ -3,6 +3,7 @@ using Einstein.ChebyshevSuite, Test
 
 @testitem "gauss_chebyshev_integration_matrix" begin
     n = 10
+    tol = typetol(Float64)
     points = gauss_chebyshev_points(Float64, n, 0.0, 1.0)
     Q = gauss_chebyshev_integration_matrix(n, 0.0, 1.0)
     @test size(Q) == (n, n)
@@ -12,12 +13,12 @@ using Einstein.ChebyshevSuite, Test
     Q_scaled = gauss_chebyshev_integration_matrix(Float64, n, lower_bound, upper_bound)
     scale = (upper_bound - lower_bound) / 2
     Q_default = gauss_chebyshev_integration_matrix(n)
-    @test isapprox(Q_scaled, Q_default .* scale, rtol=1e-12)
+    @test isapprox(Q_scaled, Q_default .* scale, atol=tol)
 
     # Test integration on constant function: f = [1,1,...,1]
     f = ones(n)
     linear = Q * f
     for i in 1:n
-        @test isapprox(linear[i], points[i], rtol=1e-12)
+        @test isapprox(linear[i], points[i], atol=tol)
     end
 end
