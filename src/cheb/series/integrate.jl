ddbe2046455b56cc129e4dfc9846a99de3f0486b
Zhen Zhong <auroradysis@gmail.com>
2025-04-03 02:30:24 +0100

    Update integrate.jl

--- src/cheb/series/integrate.jl
+++ src/cheb/series/integrate.jl
@@ -1,76 +1,48 @@
 @doc raw"""
-    cheb_series_integrate(coeffs::AbstractVector{TF}) where {TF<:AbstractFloat}
-    cheb_series_integrate(::Type{TF}, n::Integer)(coeffs::AbstractVector{TF}) where {TF<:AbstractFloat}
+    cheb_series_integrate(df::AbstractVector{TF}) where {TF<:AbstractFloat}
+    cheb_series_integrate!(f::AbstractVector{TF}, df::AbstractVector{TF}) where {TF<:AbstractFloat}
 
-Compute the indefinite integral of a functio $f$ given its Chebyshev series,
+Compute the indefinite integral of a function $f'(x)$ given its Chebyshev series,
 with the constant of integration chosen such that $f(-1) = 0$.
 
 # Arguments
-- `coeffs`: Vector of Chebyshev series
+- `df`: Vector of Chebyshev series coefficients of $f'$
+- `f`: Vector of Chebyshev series coefficients of $f$
 
 # References
 - [chebfun/@chebtech/cumsum.m at master · chebfun/chebfun](https://github.com/chebfun/chebfun/blob/master/%40chebtech/cumsum.m)
 """
-struct ChebyshevSeriesIntegrationPlan{TF<:AbstractFloat,TI<:Integer}
-    n::TI               # Number of coefficients
-    tmp::Vector{TF}     # Temporary storage for padded coefficients
-    result::Vector{TF}  # Result storage
-    weights::Vector{TI} # Pre-computed alternating signs
-
-    function ChebyshevSeriesIntegrationPlan{TF}(n::TI) where {TF<:AbstractFloat,TI<:Integer}
-        # Pre-allocate workspace
-        tmp = Vector{TF}(undef, n + 2)
-        result = Vector{TF}(undef, n + 1)
-
-        # Pre-compute alternating signs [1, -1, 1, -1, ...]
-        weights = ones(TI, n)
-        weights[2:2:end] .= -one(TI)
-
-        return new{TF,TI}(n, tmp, result, weights)
-    end
-end
-
-function (op::ChebyshevSeriesIntegrationPlan{TF,TI})(
-    coeffs::AbstractVector{TF}
-) where {TF<:AbstractFloat,TI<:Integer}
-    @argcheck length(coeffs) == op.n "length(coeffs) must be equal to n"
-
-    (; n, tmp, result, weights) = op
-
-    # Pad with zeros
-    tmp[1:n] .= coeffs
-    tmp[(n + 1):(n + 2)] .= 0
-
-    # Compute interior coefficients
-    @inbounds begin
-        # b₂ = c₁ - c₃/2
-        result[2] = tmp[1] - tmp[3] / 2
-
-        # bᵣ = (cᵣ₋₁ - cᵣ₊₁)/(2r) for r > 1
-        for r in 2:n
-            result[r + 1] = (tmp[r] - tmp[r + 2]) / (2 * r)
-        end
+function cheb_series_integrate!(
+    f::AbstractVector{TF}, df::AbstractVector{TF}
+) where {TF<:AbstractFloat}
+    @boundscheck begin
+        @argcheck length(f) == length(df) + 1 "length(f) must be equal to length(df) + 1"
+        @argcheck firstindex(f) == 1 "firstindex(f) must be 1"
+        @argcheck firstindex(df) == 1 "firstindex(df) must be 1"
     end
 
-    # Compute b₀ to ensure coeffs(-1) = 0
-    @inbounds begin
-        result[1] = 0
-        for i in 1:n
-            result[1] += weights[i] * result[i + 1]
-        end
+    n = length(df)
+
+    f[2] = df[1] - df[3] / 2
+    for r in 2:(n - 2)
+        f[r + 1] = (df[r] - df[r + 2]) / (2 * r)
     end
+    f[n] = df[n - 1] / (2 * (n - 1))
+    f[n + 1] = df[n] / (2 * n)
 
-    return result
+    b0 = 0
+    for r in 1:n
+        b0 += (-1)^(r + 1) * f[r + 1]
+    end
+    f[1] = b0
+
+    return f
 end
 
-function chebyshevt_integrate_plan(::Type{TF}, n::Integer) where {TF<:AbstractFloat}
-    return ChebyshevSeriesIntegrationPlan{TF}(n)
+function cheb_series_integrate(df::AbstractVector{TF}) where {TF<:AbstractFloat}
+    f = Array{TF}(undef, length(df) + 1)
+    cheb_series_integrate!(f, df)
+    return f
 end
 
-function cheb_series_integrate(coeffs::AbstractVector{TF}) where {TF<:AbstractFloat}
-    n = length(coeffs)
-    op = ChebyshevSeriesIntegrationPlan{TF}(n)
-    return op(coeffs)
-end
-
-export cheb_series_integrate
+export cheb_series_integrate, cheb_series_integrate!
