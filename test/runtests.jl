2dc3a37af376fd793f6da3a79e7b0b5a239bc8de
Zhen Zhong <auroradysis@gmail.com>
2025-04-03 20:46:48 +0100

    add JET.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,4 +7,10 @@ using TestItemRunner
     Aqua.test_all(Einstein)
 end
 
+@testitem "Code linting (JET.jl)" begin
+    using JET, Einstein
+
+    JET.test_package(Einstein; target_defined_modules=true)
+end
+
 @run_package_tests
