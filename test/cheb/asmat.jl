63cb36537ad9baa151bce5bae703ab3ddeea633b
Zhen Zhong <auroradysis@gmail.com>
2025-04-03 18:58:01 +0100

    fix tests

--- test/cheb/asmat.jl
+++ test/cheb/asmat.jl
@@ -1,11 +1,14 @@
-@testitem "cheb_gauss_vals2coeffs_matrix, cheb_gauss_coeffs2vals_matrix" begin
-    for type in [Float64, BigFloat]
-        tol = typetol(type)
+@testitem "cheb_vals2coeffs_matrix, cheb_coeffs2vals_matrix" begin
+    using LinearAlgebra
 
-        @testset "matrix properties" begin
+    tol = typetol(Float64)
+    grid_list = [ChebyshevGaussGrid, ChebyshevLobattoGrid]
+
+    for grid in grid_list
+        @testset "$grid, matrix properties" begin
             for n in 1:10
-                A = cheb_gauss_vals2coeffs_matrix(type, n)
-                S = cheb_gauss_coeffs2vals_matrix(type, n)
+                A = grid.vals2coeffs_matrix(n)
+                S = grid.coeffs2vals_matrix(n)
 
                 # Test matrix dimensions
                 @test size(A) == (n, n)
@@ -14,29 +17,25 @@
                 # Test that matrices are inverses of each other
                 @test isapprox(A * S, Matrix(I, n, n), atol=tol)
                 @test isapprox(S * A, Matrix(I, n, n), atol=tol)
-
-                # Test that matrices are symmetric
-                @test isapprox(A, A', atol=tol)
-                @test isapprox(S, S', atol=tol)
             end
         end
 
-        @testset "polynomial interpolation" begin
+        @testset "$grid, polynomial interpolation" begin
             for n in 1:10
                 # Create a polynomial function
                 f(x) = 3x^2 + 2x - 1
-                
+
                 # Get points and evaluate function
-                x = cheb_gauss_points(type, n)
+                x = grid.points(n)
                 f_vals = f.(x)
-                
+
                 # Transform to coefficients and back
-                A = cheb_gauss_vals2coeffs_matrix(type, n)
-                S = cheb_gauss_coeffs2vals_matrix(type, n)
-                
+                A = grid.vals2coeffs_matrix(n)
+                S = grid.coeffs2vals_matrix(n)
+
                 f_coeffs = A * f_vals
                 f_vals_reconstructed = S * f_coeffs
-                
+
                 # Test reconstruction
                 @test isapprox(f_vals, f_vals_reconstructed, atol=tol)
             end
@@ -46,18 +45,18 @@
             for n in 1:10
                 # Create a trigonometric function
                 f(x) = sin(π * x) * cos(2π * x)
-                
+
                 # Get points and evaluate function
-                x = cheb_gauss_points(type, n)
+                x = grid.points(n)
                 f_vals = f.(x)
-                
+
                 # Transform to coefficients and back
-                A = cheb_gauss_vals2coeffs_matrix(type, n)
-                S = cheb_gauss_coeffs2vals_matrix(type, n)
-                
+                A = grid.vals2coeffs_matrix(n)
+                S = grid.coeffs2vals_matrix(n)
+
                 f_coeffs = A * f_vals
                 f_vals_reconstructed = S * f_coeffs
-                
+
                 # Test reconstruction
                 @test isapprox(f_vals, f_vals_reconstructed, atol=tol)
             end
